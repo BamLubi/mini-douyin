31f8c16776119cfec2ffbe4e1eb0992ac5316fad
BamLubi <1126458447@qq.com>
2023-03-02 01:59:25 -0800

    feat: idl for each micro server

---
+++ idl/user.thrift
@@ -0,0 +1,56 @@
+namespace go userdouyin
+
+include "base.thrift"
+
+struct UserRegisterRequest {
+    1:string username // 注册用户名，最长32个字符
+    2:string password // 密码，最长32个字符
+}
+
+struct UserRegisterResponse {
+    1:i32 status_code
+    2:optional string status_msg
+    3:i64 user_id
+    4:string token
+}
+
+struct UserLoginRequest {
+    1:string username
+    2:string password
+}
+
+struct UserLoginResponse {
+    1:i32 status_code
+    2:optional string status_msg
+    3:i64 user_id
+    4:string token
+}
+
+struct UserInfoRequest {
+    1:i64 user_id
+    2:string token
+}
+
+struct UserInfoResponse {
+    1:i32 status_code
+    2:optional string status_msg
+    3:base.User user
+}
+
+struct PublishListRequest {
+    1:i64 user_id
+    2:string token
+}
+
+struct PublishListResponse {
+    1:i32 status_code
+    2:optional string status_msg
+    3:list<base.Video> video_list // 用户发布的视频列表
+}
+
+service UserService {
+    UserRegisterResponse UserRegister(1:UserRegisterRequest req)
+    UserLoginResponse UserLogin(1:UserLoginRequest req)
+    UserInfoResponse UserInfo(1:UserInfoRequest req)
+    PublishListResponse PublishList(1:PublishListRequest req)
+}
---
+++ idl/video.thrift
@@ -0,0 +1,31 @@
+namespace go videodouyin
+
+include "base.thrift"
+
+struct FeedRequest {
+    1:optional i64 latest_time // 可选参数，限制返回视频的最新投稿时间戳，精确到秒，不填表示当前时间
+    2:optional string token // 可选参数，登录用户设置
+}
+
+struct FeedResponse {
+    1:i32 status_code 
+    2:optional string status_msg
+    3:list<base.Video> video_lis // 视频列表
+    4:optional i64 next_time // 本次返回的视频中，发布最早的时间，作为下次请求时的latest_time
+}
+
+struct PublishActionRequest {
+    1:string token
+    2:binary data // 视频数据
+    3:string title // 视频标题
+}
+
+struct PublishActionResponse {
+    1:i32 status_code
+    2:optional string status_msg
+}
+
+service VideoService {
+    FeedResponse Feed(1:FeedRequest req)
+    PublishActionResponse PublishAction(1:PublishActionRequest req)
+}
