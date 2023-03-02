4c91edeba37fadfccbea69a94a2e8abb9ed67484
BamLubi <1126458447@qq.com>
2023-03-02 08:10:46 -0800

    feat: PublishAction

--- cmd/api/handlers/video.go
+++ cmd/api/handlers/video.go
@@ -1,11 +1,16 @@
 package handlers
 
 import (
-	"log"
+	"context"
 	"mini-douyin/cmd/api/kitex_gen/videodouyin"
 	"mini-douyin/cmd/api/middleware"
+	"mini-douyin/cmd/api/rpc"
+	config "mini-douyin/pkg/configs"
 	"mini-douyin/pkg/consts"
+	"mini-douyin/pkg/utils"
+	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	jwt "github.com/appleboy/gin-jwt/v2"
@@ -32,12 +37,60 @@ func Feed(c *gin.Context) {
 	req := new(videodouyin.FeedRequest)
 	req.LatestTime = &latest_time
 	if userId != -1 {
-		// req.Token
+		req.UserId = &userId
 	}
-
-	log.Println(latest_time)
+	// RPC获取所有的列表
+	resp, err := rpc.VideoClient.Feed(context.Background(), req)
+	if err != nil {
+		config.Logger.Error(err.Error())
+		return
+	}
+	c.JSON(200, resp)
 }
 
-func Publish(c *gin.Context) {
+func PublishAction(c *gin.Context) {
+	// 提取参数
+	token := c.PostForm("token")
+	title := c.PostForm("title")
+	file, err := c.FormFile("data")
+	if err != nil {
+		config.Logger.Error(err.Error())
+	}
 
+	videoId := utils.IdGen()
+	videoIdString := strconv.Itoa(int(videoId))
+	videoFileName := videoIdString + "." + strings.Split(file.Filename, ".")[1]
+	coverFileName := videoIdString + ".jpg"
+	playUrl := "http://192.168.45.129:6789/static/video/" + videoFileName
+	coverUrl := "http://192.168.45.129:6789/static/cover/" + coverFileName
+	playUrlReal := "public/video/" + videoFileName
+	coverUrlReal := "public/cover/" + coverFileName
+	userID := middleware.ParseUserIdFromTokenString(token)
+
+	// 协程将文件保存在本地并创建封面图
+	go func() {
+		if err := c.SaveUploadedFile(file, playUrlReal); err != nil {
+			config.Logger.Error(err.Error())
+			return
+		}
+		// 创建首页图片
+		if err := utils.GetVideoCover(playUrlReal, coverUrlReal); err != nil {
+			config.Logger.Error(err.Error())
+			return
+		}
+		// TODO: 是否需要做失败后的操作，比如将这条数据置为失效
+	}()
+
+	req := &videodouyin.PublishActionRequest{UserId: userID, VideoId: videoId, PlayUrl: playUrl, CoverUrl: coverUrl, Title: title}
+	resq, err := rpc.VideoClient.PublishAction(context.Background(), req)
+	if err != nil {
+		config.Logger.Error(err.Error())
+		// 删除已经保存的文件
+		go func() {
+			os.Remove(playUrlReal)
+			os.Remove(coverUrlReal)
+		}()
+		return
+	}
+	c.JSON(200, resq)
 }
--- cmd/video/handler.go
+++ cmd/video/handler.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"mini-douyin/cmd/video/kitex_gen/base"
 	videodouyin "mini-douyin/cmd/video/kitex_gen/videodouyin"
+	config "mini-douyin/pkg/configs"
+	"mini-douyin/pkg/entity"
 )
 
 // VideoServiceImpl implements the last service interface defined in the IDL.
@@ -11,11 +14,50 @@ type VideoServiceImpl struct{}
 // Feed implements the VideoServiceImpl interface.
 func (s *VideoServiceImpl) Feed(ctx context.Context, req *videodouyin.FeedRequest) (resp *videodouyin.FeedResponse, err error) {
 	// TODO: Your code here...
+	var followCount int64 = 0
+	var followerCount int64 = 0
+	var DemoUser = base.User{
+		Id:            1,
+		Name:          "TestUser",
+		FollowCount:   &followCount,
+		FollowerCount: &followerCount,
+		IsFollow:      false,
+	}
+	var DemoVideos = []*base.Video{
+		{
+			Id:            1,
+			Author:        &DemoUser,
+			PlayUrl:       "https://www.w3schools.com/html/movie.mp4",
+			CoverUrl:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
+			FavoriteCount: 0,
+			CommentCount:  0,
+			IsFavorite:    false,
+		},
+	}
+	resp = &videodouyin.FeedResponse{StatusCode: 0, VideoList: DemoVideos, NextTime: req.LatestTime}
 	return
 }
 
 // PublishAction implements the VideoServiceImpl interface.
 func (s *VideoServiceImpl) PublishAction(ctx context.Context, req *videodouyin.PublishActionRequest) (resp *videodouyin.PublishActionResponse, err error) {
 	// TODO: Your code here...
-	return
+	var videoInfo entity.VideoInfo
+	videoInfo.Id = req.VideoId
+	videoInfo.UserId = req.UserId
+	videoInfo.CoverUrl = req.CoverUrl
+	videoInfo.PlayUrl = req.PlayUrl
+	videoInfo.Title = req.Title
+
+	err = config.DB.Create(&videoInfo).Error
+	if err != nil {
+		config.Logger.Error("PublishAction: " + err.Error())
+
+		err_msg := err.Error()
+		resp = &videodouyin.PublishActionResponse{StatusCode: 1, StatusMsg: &err_msg}
+		return resp, err
+	}
+
+	// 返回结果
+	resp = &videodouyin.PublishActionResponse{StatusCode: 0}
+	return resp, nil
 }
