2bb3560c3fd30aa5bf649cd00ad554f886ca03d7
BamLubi <1126458447@qq.com>
2023-03-08 22:59:08 -0800

    feat: use redis

--- cmd/socity/handler.go
+++ cmd/socity/handler.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
+	"log"
+	"math/rand"
 	socitydouyin "mini-douyin/cmd/socity/kitex_gen/socitydouyin"
 	"mini-douyin/cmd/socity/kitex_gen/userdouyin"
 	"mini-douyin/cmd/socity/rpc"
 	config "mini-douyin/pkg/configs"
 	"mini-douyin/pkg/entity"
 	"mini-douyin/pkg/utils"
+	"strconv"
 	"time"
 )
 
@@ -18,17 +21,47 @@ type SocityServiceImpl struct{}
 func (s *SocityServiceImpl) FavoriteAction(ctx context.Context, req *socitydouyin.FavoriteActionRequest) (resp *socitydouyin.FavoriteActionResponse, err error) {
 	// 无论是点赞还是取消点赞都修改数据库
 	// TODO：使用redis优化时在考虑
+	// 1. 将点赞数据先存储在redis中，用hashtable存储
+	// 2. 监听redis过期，写入数据库
+	randSecond := 1*60*60 + rand.Intn(30*60)
+	randSecond = 10 // 测试用
+	config.RD.Send("SELECT", 10)
+	config.RD.Send("SET", "user_favorite_ex_"+strconv.Itoa(int(req.UserId)), "expire", "EX", randSecond)
+	config.Logger.Info("user_favorite_ex_" + strconv.Itoa(int(req.UserId)))
+	config.RD.Send("HSET", "user_favorite_"+strconv.Itoa(int(req.UserId)), strconv.Itoa(int(req.VideoId)), strconv.Itoa(int(req.ActionType)))
 	if req.ActionType == 1 {
-		favourite := &entity.Favorite{Id: utils.IdGen(), UserId: req.UserId, VideoId: req.VideoId, ActionType: req.ActionType}
-		err = config.DB.Create(favourite).Error
+		config.RD.Send("HINCRBY", "video_"+strconv.Itoa(int(req.VideoId)), "favorite_count", 1)
 	} else {
-		err = config.DB.Model(&entity.Favorite{}).Where("user_id = ?", req.UserId).Where("video_id = ?", req.VideoId).Update("action_type", req.ActionType).Error
+		config.RD.Send("HINCRBY", "video_"+strconv.Itoa(int(req.VideoId)), "favorite_count", -1)
 	}
-	if err != nil {
-		config.Logger.Error(err.Error())
-		err_msg := "FavoriteAction DB error"
-		resp = &socitydouyin.FavoriteActionResponse{StatusCode: 1, StatusMsg: &err_msg}
-		return
+	config.RD.Send("SET", "video_ex_"+strconv.Itoa(int(req.VideoId)), "expire", "EX", 30)
+	config.RD.Flush()
+
+	defer func() {
+		if err := recover(); err != nil {
+			config.RD.Do("DEL", "user_favorite_ex_"+strconv.Itoa(int(req.UserId)))
+			config.RD.Do("DEL", "user_favorite_"+strconv.Itoa(int(req.UserId)))
+
+			config.Logger.Error(err.(string))
+			err_msg := "FavoriteAction Redis error"
+			resp = &socitydouyin.FavoriteActionResponse{StatusCode: 1, StatusMsg: &err_msg}
+		}
+	}()
+
+	if _, err := config.RD.Receive(); err != nil {
+		panic("Redis error 1 " + err.Error())
+	}
+	if _, err := config.RD.Receive(); err != nil {
+		log.Println(err)
+		panic("Redis error 2 " + err.Error())
+	}
+	if _, err := config.RD.Receive(); err != nil {
+		log.Println(err)
+		panic("Redis error 3 " + err.Error())
+	}
+	if _, err := config.RD.Receive(); err != nil {
+		log.Println(err)
+		panic("Redis error 4 " + err.Error())
 	}
 	resp = &socitydouyin.FavoriteActionResponse{StatusCode: 0}
 	return
