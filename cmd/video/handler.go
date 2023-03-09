3c47457d7aa7ed5d953383ba277bc1134f0417e1
BamLubi <1126458447@qq.com>
2023-03-08 23:21:04 -0800

    feat: read stats from redis

--- cmd/video/handler.go
+++ cmd/video/handler.go
@@ -5,6 +5,7 @@ import (
 	videodouyin "mini-douyin/cmd/video/kitex_gen/videodouyin"
 	config "mini-douyin/pkg/configs"
 	"mini-douyin/pkg/entity"
+	"strconv"
 	"time"
 )
 
@@ -27,6 +28,47 @@ func (s *VideoServiceImpl) Feed(ctx context.Context, req *videodouyin.FeedReques
 		resp = &videodouyin.FeedResponse{StatusCode: 0}
 		return
 	}
+	// 查询redis是否需要更新统计信息
+	// 如果redis中有，则添加入返回值
+	// 如果没有，则从mysql插入redis
+	config.Logger.Debug("运行到这，获取redis")
+	for i := 0; i < len(videoListIDL); i++ {
+		videoId := strconv.Itoa(int(videoListIDL[i].Id))
+		config.Logger.Debug("vide id" + videoId)
+
+		config.RD.Send("SELECT", 10)
+		config.RD.Send("HGET", "video_"+videoId, "favorite_count")
+		config.RD.Send("HGET", "video_"+videoId, "comment_count")
+		config.RD.Flush()
+		v, _ := config.RD.Receive()
+		v, _ = config.RD.Receive()
+		if v == nil {
+			config.RD.Send("SELECT", 10)
+			config.RD.Send("HSET", "video_"+videoId, "favorite_count", videoListIDL[i].FavoriteCount)
+			config.RD.Flush()
+		} else {
+			str := ""
+			for _, n := range v.([]uint8) {
+				str += string(n)
+			}
+			config.Logger.Debug("favorite_count " + str)
+			videoListIDL[i].FavoriteCount, _ = strconv.ParseInt(str, 10, 64)
+		}
+		v, _ = config.RD.Receive()
+		if v == nil {
+			config.RD.Send("SELECT", 10)
+			config.RD.Do("HSET", "video_"+videoId, "comment_count", videoListIDL[i].CommentCount)
+			config.RD.Flush()
+		} else {
+			str := ""
+			for _, n := range v.([]uint8) {
+				str += string(n)
+			}
+			config.Logger.Debug("comment_count " + str)
+			videoListIDL[i].CommentCount, _ = strconv.ParseInt(str, 10, 64)
+		}
+	}
+
 	var lastTime int64 = videoListIDL[len(videoListIDL)-1].CreateTime
 	resp = &videodouyin.FeedResponse{StatusCode: 0, VideoList: videoListIDL, NextTime: &lastTime}
 
